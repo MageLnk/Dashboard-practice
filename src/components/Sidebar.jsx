16024c3e2b5e2107534c3c7d81191ece887bc0b9
MageLink <mage.link.zhezard@gmail.com>
2022-06-22 08:41:26 -0400

    NavBar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,11 @@ import { useStateContext } from "../contexts/ContextProvider";
 // Main
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) setActiveMenu(false);
+  };
 
   const activeLink =
     "flex items-cente gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -23,7 +27,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoopy</span>
@@ -48,7 +52,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
