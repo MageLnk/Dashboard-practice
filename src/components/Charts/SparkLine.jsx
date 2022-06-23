f975898d54db096a3be032dd333b535313f7ca8e
MageLink <mage.link.zhezard@gmail.com>
2022-06-23 08:46:56 -0400

    Making the sparkline

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,4 +1,10 @@
-const SparkLine = () => {
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+// App
+const SparkLine = ({ id, data, type, color, width, height, currentColor }) => {
   return <div>SparkLine</div>;
 };
 
