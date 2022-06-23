7626762b87cfc8e36b8377b0508f1be98eaec334
MageLink <mage.link.zhezard@gmail.com>
2022-06-23 06:38:10 -0400

    Homepage working

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
